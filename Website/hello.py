3e4a034ad888eb81e0aa6113ecb8b3f8e206a07f
Reimar Tria <reimartria@MacBook-Air-2.local>
2021-01-19 14:13:58 -0800

    Website_Stuff.html

--- Website/hello.py
+++ Website/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '''Hello, Country! <i>this is emphasized</i> 
+    return '''Hello, Country! <i>this is not emphasized</i> 
     
     
     
